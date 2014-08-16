854491cb1942e5a0822c18670e2a1f12fa1614c3
Adrian Sampson <adrian@radbox.org>
2014-08-16 14:26:12 -0700

    Demonstrate arbitrary qualifier locations

--- examples/nullness/test/simple.c
+++ examples/nullness/test/simple.c
@@ -10,6 +10,17 @@ int main() {
   b = 0;
   a = b;  // expected-warning {{may become null}}
   b = a;
+  *a = 1;
+  *b = 1;  // expected-warning {{dereferencing nullable}}
+
+  // Here's something that non-type-based checkers can't do.
+  int * NULLABLE *c;
+  int ** NULLABLE d;
+  c = 0;  // expected-warning {{may become null}}
+  d = 0;
+  *c = 0;
+  *d = 0;  // expected-warning {{may become null}} \
+      expected-warning {{dereferencing nullable}}
 
   return 0;
 }

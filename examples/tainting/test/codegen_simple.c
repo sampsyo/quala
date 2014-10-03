e9a2e8ce1fb60945eae760df727e24da7727376b
Adrian Sampson <adrian@radbox.org>
2014-10-03 15:22:52 -0700

    Initial codegen tests

---
+++ examples/tainting/test/codegen_simple.c
@@ -0,0 +1,24 @@
+// RUN: clang -Xclang -verify -emit-llvm -S -o - %s | FileCheck %s
+// expected-no-diagnostics
+
+#define TAINTED __attribute__((type_annotate("tainted")))
+
+int main() {
+  // CHECK: %x = alloca i32, align 4, !tyann [[TAINTED:![0-9]+]]
+  // CHECK: store i32 2, i32* %x, align 4, !tyann [[TAINTED]]
+  TAINTED int x = 2;
+
+  // CHECK: %y = alloca i32, align 4
+  // CHECK: store i32 3, i32* %x, align 4
+  int y = 3;
+
+  // CHECK: %z = alloca i32, align 4, !tyann [[TAINTED]]
+  TAINTED int z;
+
+  // CHECK: %add = add nsw i32 %{{[0-9]+}}, %{{[0-9]+}}, !tyann [[TAINTED]]
+  z = x + y;
+
+  return 0;
+}
+
+// CHECK: [[TAINTED]] = metadata !{metadata !"tainted", i8 0}

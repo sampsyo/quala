1818aeb2c1ed3cb4c7b0e2f9191771a971be565f
Adrian Sampson <adrian@radbox.org>
2014-10-03 16:34:03 -0700

    Fix up tests according to alloca order, etc.

--- examples/tainting/test/codegen_simple.c
+++ examples/tainting/test/codegen_simple.c
@@ -5,17 +5,17 @@
 
 int main() {
   // CHECK: %x = alloca i32, align 4, !tyann [[TAINTED:![0-9]+]]
+  // CHECK: %y = alloca i32, align 4
+  // CHECK: %z = alloca i32, align 4, !tyann [[TAINTED]]
+
   // CHECK: store i32 2, i32* %x, align 4, !tyann [[TAINTED]]
   TAINTED int x = 2;
 
-  // CHECK: %y = alloca i32, align 4
-  // CHECK: store i32 3, i32* %x, align 4
+  // CHECK: store i32 3, i32* %y, align 4
   int y = 3;
 
-  // CHECK: %z = alloca i32, align 4, !tyann [[TAINTED]]
-  TAINTED int z;
-
   // CHECK: %add = add nsw i32 %{{[0-9]+}}, %{{[0-9]+}}, !tyann [[TAINTED]]
+  TAINTED int z;
   z = x + y;
 
   return 0;
